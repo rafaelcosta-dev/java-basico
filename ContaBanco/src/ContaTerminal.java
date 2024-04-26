751d06dec54e6d13962d5eb4b32c9341a4ae570f
Rafael <126898596+rafaelcosta-dev@users.noreply.github.com>
2024-04-26 16:15:19 -0300

    att
    att scanner text error

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
+        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
 
         int contaBancaria;
