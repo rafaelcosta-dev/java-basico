46dd7ad5ca346d67bd5ec2cb3cab3fa46b3eddbd
Rafael <126898596+rafaelcosta-dev@users.noreply.github.com>
2024-05-09 16:52:31 -0300

    desafio dio complete
    att

---
+++ Testes/teste.java
@@ -0,0 +1,17 @@
+package Testes;
+
+public class teste {
+
+    public static void main(String[] args){
+
+        for( int carneiros = 1; carneiros <= 20; carneiros ++){
+
+            System.out.println("Contando:" + carneiros);
+        }
+
+        System.out.println("Dormiu");
+    
+
+    }
+    
+}
